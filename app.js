a84a4c9dcf7296b3a89139b2c8e940e66db66e61
ramadevikodi <109166730+ramadevikodi@users.noreply.github.com>
2024-10-22 07:47:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
-/*
+// /*
 
-GEREKLİ PAKETLER YÜKLENİYOR...
+// GEREKLİ PAKETLER YÜKLENİYOR...
 
-*/
-var http = require('http');
-var express = require('express');
+// */
+// var http = require('http');
+// var express = require('express');
 
-var app = express();
+// var app = express();
 
-app.set('port', process.env.PORT || 3005); // GİRİŞ PORTU AYARLANDI
-app.set('views', __dirname + '/app/server/views'); // VIEW KLASÖRÜ TANITILDI
-app.set('view engine', 'ejs'); // VIEW ENGINE AYARLANDI
-app.use(express.static(__dirname + '/app/public')); // KULLANICILAR TARAFINDAN ERİŞİLEBİLEN KLASÖR TANIMLANDI
+// app.set('port', process.env.PORT || 3005); // GİRİŞ PORTU AYARLANDI
+// app.set('views', __dirname + '/app/server/views'); // VIEW KLASÖRÜ TANITILDI
+// app.set('view engine', 'ejs'); // VIEW ENGINE AYARLANDI
+// app.use(express.static(__dirname + '/app/public')); // KULLANICILAR TARAFINDAN ERİŞİLEBİLEN KLASÖR TANIMLANDI
 
-require('./app/routes')(app); // ROUTE DOSYASI ÇAĞIRILDI
+// require('./app/routes')(app); // ROUTE DOSYASI ÇAĞIRILDI
 
-/*
+// /*
 
-HTTP SERVER OLUŞTURULDU
+// HTTP SERVER OLUŞTURULDU
 
-*/
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Sistem ' + app.get('port') + ' Portu Üzerinde Çalışıyor.');
+// */
+// http.createServer(app).listen(app.get('port'), function(){
+// 	console.log('Sistem ' + app.get('port') + ' Portu Üzerinde Çalışıyor.');
+// });
+
+import express from 'express';
+
+import connectToDatabase from './helpers.mjs'
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h2>Hi there!</h2>');
 });
+
+await connectToDatabase();
+
+app.listen(3000);
